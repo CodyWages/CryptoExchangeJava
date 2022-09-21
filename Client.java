e9a96f90e8c2128b1cf9663bba0957c55fb1943e
Cody Wages <92700624+CodyWages@users.noreply.github.com>
2022-09-20 20:00:50 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,34 +5,34 @@ public class Client {
     public static Scanner scan = new Scanner(System.in);
     public String clientID;
 
-    //Client Asset Balance
+    // Client Asset Balance
     double usdBalance = 1000000.00;
     double btcBalance = 0.00;
     double ethBalance = 0.00;
 
-    //Client Functions
-    //Input Client ID
+    // Client Functions
+    // Input Client ID
     public void inputClientID() {
         System.out.print("Please input your username: ");
         clientID = scan.nextLine();
     }
 
-    //Return Client ID
+    // Return Client ID
     public String clientID() {
         return clientID;
     }
 
-    //Return Client US Dollar Balance
+    // Return Client US Dollar Balance
     public double usdBalance() {
         return usdBalance;
     }
     
-    //Return Client Bitcoin Balance
+    // Return Client Bitcoin Balance
     public double btcBalance() {
         return btcBalance;
     }
     
-    //Return Client Ethereum Balance
+    // Return Client Ethereum Balance
     public double ethBalance() {
         return ethBalance;
     }
